47881bad0137d6d19a133d17e39846c30ad23a0f
Benjamin Purgason <nullrouteftw@gmail.com>
2013-05-04 09:53:10 -0700

    Adding TorrentTest

---
+++ TorrentTest/TorrentTest.py
@@ -0,0 +1,70 @@
+'''
+Created on May 4, 2013
+
+@author: bpurgaso
+'''
+
+import libtorrent
+import time
+import sys
+import os
+
+
+class Client(object):
+    def __init__(self):
+        self.state_string = ['queued',
+                             'checking',
+                             'downloading metadata',
+                             'downloading',
+                             'finished',
+                             'seeding',
+                             'allocating',
+                             'checking fastresume']
+
+        self.progress_string = '\r{0:3.2f}% complete (down: {1:.1f}% kb/s up:'\
+                               ' {2:.1f}% kb/s peers: {3}) {4}'
+        self.update_delay = 1
+
+    def getUpdateFactory(self, handle):
+        while not handle.is_seed():
+            s = handle.status()  # update status
+            time.sleep(self.update_delay)
+            yield self.progress_string.format(s.progress * 100,
+                                             s.download_rate / 1000,
+                                             s.upload_rate / 1000,
+                                             s.num_peers,
+                                             self.state_string[s.state])
+
+    def download_torrent(self, torrent_path, mode='console'):
+        if mode != 'console' and mode != 'generator':
+            raise(Exception('InvalidMode'))
+        session = libtorrent.session()
+        session.listen_on(6881, 6891)
+        info = libtorrent.torrent_info(sys.argv[1])
+        handle = session.add_torrent({'ti': info, 'save_path': './'})
+
+        print 'starting', handle.name()
+        for i in self.getUpdateFactory(handle):
+            sys.stdout.write(i)
+            sys.stdout.flush()
+        print handle.name(), 'complete'
+
+    def download_magnet(self, link, mode='console'):
+        if mode != 'console' and mode != 'generator':
+            raise(Exception('InvalidMode'))
+        session = libtorrent.session()
+        session.listen_on(6881, 6891)
+        handle = libtorrent.add_magnet_uri(session, link, {'save_path': './'})
+
+        print 'starting', handle.name()
+        for i in self.getUpdateFactory(handle):
+            sys.stdout.write(i)
+            sys.stdout.flush()
+        print handle.name(), 'complete'
+
+c = Client()
+target = sys.argv[1]
+if os.path.isfile(target):
+    c.download_torrent(target)
+else:
+    c.download_magnet(target)
